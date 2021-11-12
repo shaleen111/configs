28f2350d6643c6686fbe9bec03f7f8195c8d3bfe
Shaleen Baral <shaleenbaral111@gmail.com>
2021-11-12 14:42:18 +0545

    Added Powershell Configs

---
+++ powershell/Install.ps1
@@ -0,0 +1,4 @@
+Install-Module oh-my-posh -Scope CurrentUser
+Install-Module posh-git -Scope CurrentUser
+Install-Module -Name Terminal-Icons -Repository PSGallery
+
---
+++ powershell/Profile.ps1
@@ -0,0 +1,5 @@
+Import-Module oh-my-posh
+Import-Module -Name Terminal-Icons
+Set-PoshPrompt -Theme mojada
+$env:TERM='xterm-256color'
+$env:NEO='Path\to\Neovim\Config\Folder'
